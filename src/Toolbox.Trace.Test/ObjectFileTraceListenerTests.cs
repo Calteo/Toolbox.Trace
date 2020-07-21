c99a0cdaa78f874ec39fba293d5e2a339d9c5b53
Calteo <matthias@famger.de>
2020-07-21 22:07:30 +0200

    correct date time pattern in tests

--- src/Toolbox.Trace.Test/ObjectFileTraceListenerTests.cs
+++ src/Toolbox.Trace.Test/ObjectFileTraceListenerTests.cs
@@ -63,6 +63,7 @@ namespace Toolbox.Trace.Test
                 {
                     var line = reader.ReadLine()
                         .Replace(".", @"\.")
+                        .Replace("*", @".*")
                         .Replace("[", @"\[").Replace("]", @"\]")
                         .Replace("(", @"\(").Replace(")", @"\)")
                         .Replace("<", @"\<").Replace(">", @"\>");
